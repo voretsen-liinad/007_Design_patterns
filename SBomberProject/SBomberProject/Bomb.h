ed77a6e48820dc71470ba23377e2a3cb369761a8
voretsen-liinad <maidanov1991@yandex.ru>
2022-11-24 00:07:18 +0500

    L02

--- SBomberProject/SBomberProject/Bomb.h
+++ SBomberProject/SBomberProject/Bomb.h
@@ -1,5 +1,8 @@
 #pragma once
 
+#include <iostream>
+#include "MyTools.h"
+
 #include "DynamicObject.h"
 
 class Bomb : public DynamicObject
@@ -14,3 +17,33 @@ private:
 
 };
 
+class BombDecorator : public DynamicObject {
+public:
+	
+	void Move(uint16_t time) override {
+		bomb.Move(time * 1.6);
+	}
+
+	void Draw() const override {
+		bomb.Draw();
+		MyTools::ScreenSingleton::getInstance().GotoXY(bomb.GetX(), bomb.GetY() - 1);
+		std::cout << "|";
+	}
+
+	inline void SetPos(double nx, double ny) override { bomb.SetPos(nx, ny); }
+
+	inline double GetY() const override { return bomb.GetY(); }
+	
+	inline double GetX() const override { return bomb.GetX();; }
+
+	inline void SetWidth(uint16_t widthN) override { bomb.SetWidth(widthN); }
+
+	inline uint16_t GetWidth() const override { return bomb.GetWidth(); }
+
+	inline void SetSpeed(double sp) override { bomb.SetSpeed(sp); }
+	
+	inline void SetDirection(double dx, double dy) override { bomb.SetDirection(dx, dy); }
+
+private:
+	Bomb bomb;
+};
--- SBomberProject/SBomberProject/DynamicObject.h
+++ SBomberProject/SBomberProject/DynamicObject.h
@@ -10,8 +10,8 @@ public:
 
     DynamicObject() : speed(0.0), xDirction(0.0), yDirection(0) { }
 
-    inline void SetSpeed(double sp) { speed = sp; }
-    inline void SetDirection(double dx, double dy) { xDirction = dx; yDirection = dy; }
+    virtual inline void SetSpeed(double sp) { speed = sp; }
+    virtual inline void SetDirection(double dx, double dy) { xDirction = dx; yDirection = dy; }
     
     virtual void Move(uint16_t time) { x += xDirction * speed * time * 0.001; y += yDirection * speed * time * 0.001; };
 
